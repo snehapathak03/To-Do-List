7a3fc0b4d03fd2d853f1db6d38917192b17b667f
snehapathak03 <snehapathak1303@gmail.com>
2026-04-06 21:30:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const inputBox = document.getElementById('inputBox');
+const addButton = document.getElementById('addButton');
+const todoList = document.getElementById('todoList');
+
+const addTodo =()=>{
+    const inputText = inputBox.value.trim();
+}
+
+addButton.addEventListener('click', addTodo);
+
