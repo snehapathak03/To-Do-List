9ab61d5fbdadb84508591efe31547eea10c6b7cf
snehapathak03 <snehapathak1303@gmail.com>
2026-04-08 23:08:14 +0530

    Update html, css, js files

--- script.js
+++ script.js
@@ -4,7 +4,32 @@ const todoList = document.getElementById('todoList');
 
 const addTodo =()=>{
     const inputText = inputBox.value.trim();
+    if(inputText.length <=0){
+        alert("You must add a task in your to-do");
+        return false;
+    }
+    // Creating p tag
+   const li = document.createElement("li");
+   const p = document.createElement("p");
+   p.innerHTML = inputText;
+   li.appendChild(p);
+   
+   //Creating delete Button
+   const deleteButton= document.createElement("button");
+   deleteButton.innerText= "REMOVE";
+   deleteButton.classList.add("button","deleteButton");
+   li.appendChild(deleteButton);
+
+   //Creating Edit Button
+   const editButton = document.createElement("button");
+   editButton.innerText= "EDIT";
+   editButton.classList.add("button", "editButton");
+   li.appendChild(editButton);
+   todoList.appendChild(li);
+   inputBox.value= ""; 
 }
+
+
 
 addButton.addEventListener('click', addTodo);
 
